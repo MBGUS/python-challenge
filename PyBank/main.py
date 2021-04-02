e2f8843c6168723aeb22a39ac93e639a698a33c5
Gustavo M <gusmaldonadob@gmail.com>
2021-04-02 08:08:38 -0600

    second part with comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,13 +6,13 @@ import csv
 pyBank_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 
 # Declare all the variables with their initial value
-total_month = 0
+total_month = 0     #Declares in 0 because it's the value of the first iteration
 total_pl = 0
 change = 0
-change_list = []
+change_list = []    #I declare a list for saving all the changes
 previous = 0
 max_num = 0
-max_dt = ""
+max_dt = ""         #As the value of dates is string, I declare as ""
 min_num = 0
 min_dt = ""
 
@@ -28,21 +28,22 @@ with open(pyBank_csv, newline='', encoding='utf-8') as pyBank_file:
     #Iteration for each row to make the calculations
     for row in pyBank_reader:
 
-        total_month = total_month + 1
-        total_pl = total_pl + int(row[1])
-        change = int(row[1]) - previous
-        change_list.append(change)
-        previous = int(row[1])
+        total_month = total_month + 1           #Sum of total months, iteration will sum 1 by 1
+        total_pl = total_pl + int(row[1])       #Same as up but convertig the row[1] as integer value to be able to sum
+        change = int(row[1]) - previous         #Formula = Initial Value - Previous Value
+        change_list.append(change)              #Save all the changes in a list
+        previous = int(row[1])                  #Re-adjust the previous value for the next iteration
 
-        if change > max_num:
+        if change > max_num:                    #The max_num is a variable wich finds the maximuy value
             max_num = change 
             max_dt = row[0]
 
-        if change < min_num:
+        if change < min_num:                    #The min_num is a variable wich finds the maximuy value
             min_num = change 
             min_dt = row[0]
 
-    average = sum(change_list[1:])/len(change_list[1:]) 
+    average = sum(change_list[1:])/len(change_list[1:])     #Average sill sum all values/total ítems
+                                                            #The function [1:] help to initiate the calculus in the value that I declare before the :
 
 # Print the results
 print("\n")
